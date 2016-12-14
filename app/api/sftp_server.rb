70100716e0aef1deca6ed805b5f495236b3cea7c
izumix33 <takahiro.izumikawa@f-scratch.com>
2016-12-14 18:48:24 +0900

    fix

--- app/api/sftp_server.rb
+++ app/api/sftp_server.rb
@@ -22,7 +22,7 @@ module SftpServer
         no = CSV.read(passwd_file, headers:false, col_sep:':').reduce(100000){|max, row| [max, row[2].to_i].max}
         setup_user = []
         setup_user << "echo #{username}:x:#{no}:#{no}::/mnt/efs/#{username}:/bin/bash | sudo tee --append #{passwd_file} > /dev/null"
-        setup_user << "echo #{username}:x:#{no} | sudo tee --append /etc/group > /dev/null"
+        setup_user << "echo #{username}:x:#{no}: | sudo tee --append /etc/group > /dev/null"
         puts `#{setup_user.join(';')}`
         # File.open(passwd_file, 'a'){|passwd| passwd.puts "#{username}:x:#{no}:#{no}::/mnt/efs/#{username}:/bin/bash"}
         # File.open('/etc/group', 'a'){|grp| grp.puts "#{username}:x:#{no}"}
